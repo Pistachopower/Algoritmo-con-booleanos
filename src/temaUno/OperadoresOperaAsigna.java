a2415984531a1aaa7b2268dfd877465c444463f0
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2022-01-24 19:42:27 +0100

    se agrega operadores opera y asigna, es decir, expresiones para acortar el codigo. En el codigo está la explicacion de forma sencilla

---
+++ src/temaUno/OperadoresOperaAsigna.java
@@ -0,0 +1,33 @@
+package temaUno;
+
+public class OperadoresOperaAsigna {
+
+	public static void main(String[] args) {
+	
+		
+	/**
+	 * 				Operadores opera y asigna:
+	 * Lo que hace es abreviar tu código, es decir, ponerlo mas corto 
+	 * En otras palabras, acorta tu código 
+	 * 
+	 * Tenemos los siguientes operadores: +=, -=, *=, /=, %=
+	 */
+		
+	//veamos un ejemplo:
+	int edad= 70;
+	
+	//digamos que queremos hacer una operacion de la siguiente manera:
+	edad= edad +3;
+	System.out.println(edad); // esta expresión se puede acortar de una forma mas sencilla. 
+	
+	//Veamos cómo con otro ejemplo:
+	//damos el valor inicial de la variable 
+	int edad2=80;
+	
+	//La siguiente expresión es lo mismo que decir: edad= edad2 + 3;  
+	edad2+=3;
+	System.out.println(edad2);
+
+	}
+
+}
